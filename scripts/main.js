ce5c7c95a06b98c8eee66c896bb1138d51570cda
akash prasher <prince.akash.prasher@gmail.com>
2020-06-01 17:50:25 +0530

    adding content

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// let getUser = document.getElementById("userName");
+
+// getUser = write(localStorage.getItem("userName"));
+
+function setName(){
+    document.getElementById("userName").innerHTML = localStorage.getItem("userName");
+}
