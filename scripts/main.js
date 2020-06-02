4749c39218f5f0886659a51b3c9921e39b51841a
akashprasher <prince.akash.prasher@gmail.com>
2020-06-02 09:52:38 +0530

    removing null

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,9 @@
 // getUser = write(localStorage.getItem("userName"));
 
 function setName(){
+    if(localStorage.getItem("userName") === "null") {
+        localStorage.setItem("userName", "Anonymous");
+    }
     document.getElementById("userName").innerHTML = localStorage.getItem("userName");
     document.getElementById("userName_rep").innerHTML = localStorage.getItem("userName");
 }
